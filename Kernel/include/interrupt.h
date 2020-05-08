6e87862424e189aa69568e4d4c960ad8cb40b1fa
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-05-08 09:50:03 +0800

    [XingfengYang] #Feat interrupt: arm11's interrupt vector table is different from cortexm4

--- Kernel/include/interrupt.h
+++ Kernel/include/interrupt.h
@@ -3,20 +3,17 @@
 
 extern uint32_t __stack;
 
-void Reset_Handler(void);
-void SysTick_Handler(void);
-void PendSV_Handler(void);
-void NMI_Handler(void);
-void MemManage_Handler(void);
-void BusFault_Handler(void);
-void UsageFault_Handler(void);
-void HardFault_Handler(void);
-void SVC_Handler(void);
-void DebugMon_Handler(void);
-
 void interrupt_init();
 
 void enable_interrupt();
 
 void disable_interrupt();
 
+
+void Reset_Handler(void);
+void undefined_instructio(void);
+void software_interrupt(void);
+void prefetch_abort(void);
+void data_abort(void);
+void irq(void);
+void fiq(void);
--- Kernel/src/interrupt.c
+++ Kernel/src/interrupt.c
@@ -2,29 +2,18 @@
 
 typedef void (*interrupt_handler_t)(void);
 
-typedef union {
-    interrupt_handler_t isr;
-    void *stack_top;
-} vector_table_t;
+typedef interrupt_handler_t vector_table_t;
 
 __attribute__((section(".interrupt_vector_table")))
 const vector_table_t vectors[] = {
-    {.stack_top = &__stack}, // 0    Pointer to top of Stack
-    Reset_Handler,              // 1    Reset handler is called when the <RESET> button is pressed
-    NMI_Handler,                // 2    Non-Maskable Interrupt handler
-    HardFault_Handler,          // 3    Hard Fault Handler
-    MemManage_Handler,          // 4    Memory management fault Handler
-    BusFault_Handler,           // 5    Bus Fault Handler
-    UsageFault_Handler,         // 6    Usage Fault Handler
-    0,                          // 7    Reserved
-    0,                          // 8    Reserved
-    0,                          // 9    Reserved
-    0,                          // 10   Reserved
-    SVC_Handler,                // 11   SuperVisor Call Handler
-    DebugMon_Handler,           // 12   Debug Monitor Handler
-    0,                          // 13   Reserved
-    PendSV_Handler,             // 14   Pendeable interrupt driven request
-    SysTick_Handler,            // 15   SysTick Timer handler
+    Reset_Handler,
+    undefined_instruction,
+    software_interrupt,
+    prefetch_abort,
+    data_abort,
+    irq,
+    0,
+    fiq
 };
 
 void interrupt_init() {
@@ -43,38 +32,26 @@ void Reset_Handler(void){
     
 }
 
-void SysTick_Handler(void){
+void undefined_instructio(void){
 
 }
 
-void PendSV_Handler(void){
+void software_interrupt(void){
 
 }
 
-void NMI_Handler(void){
+void prefetch_abort(void){
 
 }
 
-void MemManage_Handler(void){
+void data_abort(void){
 
 }
 
-void BusFault_Handler(void){
+void irq(void){
 
 }
 
-void UsageFault_Handler(void){
-
-}
-
-void HardFault_Handler(void){
-
-}
-
-void SVC_Handler(void){
-
-}
-
-void DebugMon_Handler(void){
-
+void fiq(void){
+    
 }
