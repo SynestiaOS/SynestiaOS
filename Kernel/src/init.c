e5b3ff678c13711af273ff8ad3de98daa22eab96
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-05-09 15:57:01 +0800

    [XingfengYang] #N/A

--- Kernel/src/init.c
+++ Kernel/src/init.c
@@ -9,12 +9,18 @@ void clean_bss(uint8_t *start, uint8_t *end) {
   }
 }
 
-void kernel_main(void) {
-  const char str[] = "hello world\n";
+void helloworld(){
+    const char str[] = "hello world\n";
   uint32_t i;
-  uart_init();
-  interrupt_init();
   for (i = 0; i < sizeof(str); i++) {
     uart_putc(str[i]);
   }
+}
+
+void kernel_main(void) {
+  uart_init();
+  interrupt_init();
+
+
+  helloworld();
 }
