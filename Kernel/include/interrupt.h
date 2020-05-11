03683b96b1d3909c5fb09fc7bd39ec2263feec47
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-05-11 09:46:37 +0800

    Revert "implement Timer (won't work on VM)"
    This reverts commit 742d4a1f

--- Kernel/include/interrupt.h
+++ Kernel/include/interrupt.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <../../Board/RaspberryPi3/include/raspi3.h>
 #include <stdint.h>
 
 #define ARM_INTERRUPT_REGISTER_BASE 0x7E00B000
@@ -20,45 +19,8 @@ typedef struct {
 
 rpi_irq_controller_t* getIRQController(void);
 
-#define INTERRUPTS_BASE (PERIPHERAL_BASE + INTERRUPTS_OFFSET)
-#define INTERRUPTS_PENDING (INTERRUPTS_BASE + 0x200)
-
-#define IRQ_IS_BASIC(x) ((x >= 64 ))
-#define IRQ_IS_GPU2(x) ((x >= 32 && x < 64 ))
-#define IRQ_IS_GPU1(x) ((x < 32 ))
-#define IRQ_IS_PENDING(regs, num) ((IRQ_IS_BASIC(num) && ((1 << (num-64)) & regs->IRQ_basic_pending)) || (IRQ_IS_GPU2(num) && ((1 << (num-32)) & regs->IRQ_pending_2)) || (IRQ_IS_GPU1(num) && ((1 << (num)) & regs->IRQ_pending_1)))
-#define NUM_IRQS 72
-
-__inline__ int INTERRUPTS_ENABLED(void) {
-    int res;
-    __asm__ __volatile__("mrs %[res], CPSR": [res] "=r" (res)::);
-    return ((res >> 7) & 1) == 0;
-}
-
-__inline__ void ENABLE_INTERRUPTS(void) {
-    if (!INTERRUPTS_ENABLED()) {
-        __asm__ __volatile__("cpsie i");
-    }
-}
-
-__inline__ void DISABLE_INTERRUPTS(void) {
-    if (INTERRUPTS_ENABLED()) {
-        __asm__ __volatile__("cpsid i");
-    }
-}
-
-typedef void (*interrupt_handler_f)(void);
-typedef void (*interrupt_clearer_f)(void);
-
-typedef enum {
-    SYSTEM_TIMER_1 = 1,
-    USB_CONTROLER = 9,
-    ARM_TIMER = 64
-} irq_number_t;
-
 void interrupt_init();
-void enable_interrupt();
-void disable_interrupt();
 
-void register_irq_handler(irq_number_t irq_num, interrupt_handler_f handler, interrupt_clearer_f clearer);
-void unregister_irq_handler(irq_number_t irq_num);
+void enable_interrupt();
+
+void disable_interrupt();
--- Kernel/src/init.c
+++ Kernel/src/init.c
@@ -1,8 +1,6 @@
-#include <../../Board/RaspberryPi3/include/timer.h>
 #include <../../Board/RaspberryPi3/include/uart.h>
 #include <interrupt.h>
 #include <stdint.h>
-#include <stdlib.h>
 
 void clean_bss(uint8_t *start, uint8_t *end) {
   uint32_t i;
@@ -11,8 +9,18 @@ void clean_bss(uint8_t *start, uint8_t *end) {
   }
 }
 
+void helloworld(){
+const char str[] = "hello world\n";
+  uint32_t i;
+  for (i = 0; i < sizeof(str); i++) {
+    uart_putc(str[i]);
+  }
+}
+
 void kernel_main(void) {
   uart_init();
   interrupt_init();
-  timer_init();
+
+
+  helloworld();
 }
