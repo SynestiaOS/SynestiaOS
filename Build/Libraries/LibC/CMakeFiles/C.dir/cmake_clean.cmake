2905667d5f467c8312e9fe226b32d742d9184e29
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-12 15:25:19 +0800

    #N/A update .gitignore

--- Build/Libraries/LibC/CMakeFiles/C.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/C.dir/src/stdlib.c.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
