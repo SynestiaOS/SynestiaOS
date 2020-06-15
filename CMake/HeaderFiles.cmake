d10ab9b4510bf15965abdd3c9266a330cb9673cf
shifu <wooshifu@outlook.com>
2020-06-15 13:43:38 +0800

    add vmm

--- CMake/HeaderFiles.cmake
+++ CMake/HeaderFiles.cmake
@@ -9,3 +9,7 @@ endfunction()
 function(target_include_kernel_header_files Target)
     target_include_directories(${Target} PRIVATE ${SYNESTIAOS_SOURCE_CODE_DIR}/Kernel/include)
 endfunction()
+
+function(target_include_vmm_header_files Target)
+    target_include_directories(${Target} PRIVATE ${SYNESTIAOS_SOURCE_CODE_DIR}/Kernel/Arch/${SynestiaArch}/vmm/include)
+endfunction()
