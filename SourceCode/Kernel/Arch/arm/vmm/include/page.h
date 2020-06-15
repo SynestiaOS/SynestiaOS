5acd494adb0a62a2a29aea7270cc988a9c6c1b40
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-15 12:39:19 +0800

    [NeoYang] #VMM: add vmm header

---
+++ SourceCode/Kernel/Arch/arm/vmm/include/page.h
@@ -0,0 +1,8 @@
+//
+// Created by XingfengYang on 2020/6/15.
+//
+
+#ifndef SYNESTIAOS_PAGE_H
+#define SYNESTIAOS_PAGE_H
+
+#endif //SYNESTIAOS_PAGE_H
---
+++ SourceCode/Kernel/Arch/arm/vmm/src/vmm.c
@@ -0,0 +1,5 @@
+//
+// Created by XingfengYang on 2020/6/15.
+//
+
+#include <page.h>
