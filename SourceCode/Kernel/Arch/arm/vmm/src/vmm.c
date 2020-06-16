8ef0f410c5ba5dd7f0b7f938317e012517f280b8
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-16 20:45:33 +0800

    [XingfengYang] #Document kernel vm map

--- SourceCode/Kernel/Arch/arm/vmm/src/vmm.c
+++ SourceCode/Kernel/Arch/arm/vmm/src/vmm.c
@@ -21,14 +21,14 @@ void map_kernel_mm() {
     l1Pt->l1Pte = (L1PTE *) pageTablePhysicalAddress;
     l1Pt->l1Pte[0].level2Address = pageTablePhysicalAddress + 3 * sizeof(L1PTE);
 
-    // map 64 level 2 entry
+    // map 512 level 2 entry
     L2PT *l2Pt = (L2PT *) l1Pt->l1Pte[0].level2Address;
-    for (uint32_t i = 0; i < 64; i++) {
+    for (uint32_t i = 0; i < 512; i++) {
         l2Pt->l2Pte = (L1PTE *) l1Pt->l1Pte[0].level2Address + i * sizeof(L2PTE);
         l2Pt->l2Pte[i].pageTableAddress = l1Pt->l1Pte[0].level2Address + 64 * sizeof(L2PTE) + i * 512 * sizeof(PTE);
     }
 
-    // map 512 page table  entry
+    // map 64 * 512 page table entry
     uint32_t index = 0;
     for (uint32_t i = 0; i < 64; i++) {
         l2Pt->l2Pte = (L1PTE *) l1Pt->l1Pte[0].level2Address + i * sizeof(L2PTE);
