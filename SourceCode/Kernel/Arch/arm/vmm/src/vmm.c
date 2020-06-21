a5fbe11231de8926f4be6273bc75baedaac2b1aa
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-21 18:14:09 +0800

    [XingfengYang] #N/A

--- SourceCode/Kernel/Arch/arm/vmm/src/vmm.c
+++ SourceCode/Kernel/Arch/arm/vmm/src/vmm.c
@@ -6,30 +6,28 @@
 #include <vmm.h>
 
 
-#define KERNEL_PHYSICAL_START 3*128*MB
+#define KERNEL_PHYSICAL_START 0
 #define PAGE_SIZE 4*KB
 
 #define PAGE_TABLE_SIZE 4*MB+8*KB+16*B
-
-L1PT *l1Pt;
 
 #define PHYSICAL_PAGE_NUMBERS 1<<20
 
 PhysicalPage physicalPages[PHYSICAL_PAGE_NUMBERS];
 
-uint32_t vmm_alloc_page(){
-    for(uint32_t i = 0;i<PHYSICAL_PAGE_NUMBERS;i++){
-        if(physicalPages[i].ref_count==0){
-            physicalPages[i].ref_count+=1;
+uint32_t vmm_alloc_page() {
+    for (uint32_t i = 0; i < PHYSICAL_PAGE_NUMBERS; i++) {
+        if (physicalPages[i].ref_count == 0) {
+            physicalPages[i].ref_count += 1;
             return i;
         }
     }
 }
 
-uint32_t vmm_free_page(uint32_t page){
-    for(uint32_t i = 0;i<PHYSICAL_PAGE_NUMBERS;i++){
-        if(physicalPages[i].ref_count>0){
-            physicalPages[i].ref_count-=1;
+uint32_t vmm_free_page(uint32_t page) {
+    for (uint32_t i = 0; i < PHYSICAL_PAGE_NUMBERS; i++) {
+        if (physicalPages[i].ref_count > 0) {
+            physicalPages[i].ref_count -= 1;
             return i;
         }
     }
@@ -38,35 +36,32 @@ uint32_t vmm_free_page(uint32_t page){
 void map_kernel_mm() {
     uint32_t pageTablePhysicalAddress = &__PAGE_TABLE;
 
-    for(uint32_t i = 0;i<PAGE_TABLE_SIZE/4;i++){
-        *(uint32_t*)(pageTablePhysicalAddress+i) = 0;
-    }
-
-    l1Pt = (L1PT *) pageTablePhysicalAddress;
+    L1PT *l1Pt = (L1PT *) pageTablePhysicalAddress;
 
     // map the first level 1 entry
-    l1Pt->l1Pte = (L1PTE *) pageTablePhysicalAddress;
-    l1Pt->l1Pte[0].level2Address = pageTablePhysicalAddress + 3 * sizeof(L1PTE);
+    l1Pt->l1Pte = (L1PTE *) l1Pt;
+    l1Pt->l1Pte[0].level2Address = l1Pt + 4 * sizeof(L1PTE);
 
     // map 512 level 2 entry
-    L2PT *l2Pt = (L2PT *) l1Pt->l1Pte[0].level2Address;
+    L2PT *l2Pt = (L2PT *) (l1Pt->l1Pte[0].level2Address);
+    l2Pt->l2Pte = (L2PTE *) l2Pt;
     for (uint32_t i = 0; i < 512; i++) {
-        l2Pt->l2Pte = (L2PTE *) (l1Pt->l1Pte[0].level2Address + i * sizeof(L2PTE));
-        l2Pt->l2Pte[i].pageTableAddress = l1Pt->l1Pte[0].level2Address + 512 * sizeof(L2PTE) + i * 512 * sizeof(PTE);
+        l2Pt->l2Pte[i].pageTableAddress = l2Pt + 512 * sizeof(L2PTE) + i * 512 * sizeof(PTE);
     }
 
     // map 64 * 512 page table entry
     for (uint32_t i = 0; i < 64; i++) {
-        l2Pt->l2Pte = (L2PTE *) (l1Pt->l1Pte[0].level2Address + i * sizeof(L2PTE));
-        PTE* pte = (PTE*)l2Pt->l2Pte[i].pageTableAddress;
+        l2Pt->l2Pte[i] = *(L2PTE *) (l2Pt + i * sizeof(L2PTE));
+        PTE *pte = (PTE *) l2Pt->l2Pte[i].pageTableAddress;
 
-        for(uint32_t j = 0;j<512;j++){
+        for (uint32_t j = 0; j < 512; j++) {
             uint32_t physicalPageNumber = vmm_alloc_page();
             pte[j].page_base_address = ((KERNEL_PHYSICAL_START + physicalPageNumber * PAGE_SIZE) & 0xFFFFF000) >> 12;
             // todo: other page table entry option bits
         }
     }
 }
+
 
 void vmm_init() {
     map_kernel_mm();
@@ -75,6 +70,10 @@ void vmm_init() {
 }
 
 void vmm_enable() {
+    // 1. write ttbr0
 
+    // 2. write ttbcr, enable LPAE and use ttbr0 for all address
+
+    // 3. write dacr
 }
 
