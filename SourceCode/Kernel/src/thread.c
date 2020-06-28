ccfc92752e2c3fa33a9d95fac691a9e5cfcf3a9b
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-29 00:16:32 +0800

    [XingfengYang] #IdleThread

--- SourceCode/Kernel/src/thread.c
+++ SourceCode/Kernel/src/thread.c
@@ -3,8 +3,30 @@
 //
 
 #include <thread.h>
+#include <kstack.h>
+#include <kheap.h>
 
+
+Thread *thread_create(const char *name, ThreadStartRoutine entry, void *arg, uint32_t priority) {
+
+}
 
 Thread *thread_create_idle_thread(uint32_t cpuNum) {
+    KernelStack *kernelStack = nullptr;
+    uint32_t kernelStackAllocateState = vmm_allocate_kernel_stack(kernelStack);
+    if (kernelStack != nullptr && kernelStackAllocateState > 0) {
+        // todo: init kernel stack
+    }
+}
+
+void init_thread_struct(Thread *thread, const char *name) {
+
+}
+
+void thread_exit(uint32_t returnCode) {
+
+}
+
+void thread_kill(Thread *thread) {
 
 }
