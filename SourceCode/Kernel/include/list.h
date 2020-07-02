f1f5fe0403b4757517082fc505a5bb6616eceabd
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-02 15:50:10 +0800

    [XingfengYang] #klist

--- SourceCode/Kernel/include/list.h
+++ SourceCode/Kernel/include/list.h
@@ -5,11 +5,20 @@
 #ifndef __KERNEL_LIST_H__
 #define __KERNEL_LIST_H__
 
+#include <stdint.h>
+#include <type.h>
+
 #define getNode(ptr, type, member)((type *)((char *)(ptr) - (char *)(&(((type *)0)->member))))
 
 typedef struct ListNode {
     struct ListNode *prev;
     struct ListNode *next;
 }  __attribute__((packed)) ListNode;
+
+KernelStatus klist_append(ListNode *list, ListNode *node);
+
+ListNode *klist_remove_node(ListNode *node);
+
+uint32_t klist_size(ListNode *node);
 
 #endif //__KERNEL_LIST_H__
---
+++ SourceCode/Kernel/src/list.c
@@ -0,0 +1,16 @@
+//
+// Created by XingfengYang on 2020/7/2.
+//
+#include <list.h>
+
+KernelStatus klist_append(ListNode *list, ListNode *node) {
+
+}
+
+ListNode *klist_remove_node(ListNode *node) {
+
+}
+
+uint32_t klist_size(ListNode *node) {
+
+}
