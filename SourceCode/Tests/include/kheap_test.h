ef1580f73fd22cd0a69e6bc2274ab245cffe73f3
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-06 14:47:46 +0800

    [XingfengYang] #Test test for kheap

---
+++ SourceCode/Tests/include/kheap_test.h
@@ -0,0 +1,8 @@
+//
+// Created by XingfengYang on 2020/7/6.
+//
+
+#ifndef __KERNEL_KHEAP_TEST_H__
+#define __KERNEL_KHEAP_TEST_H__
+
+#endif //__KERNEL_KHEAP_TEST_H__
---
+++ SourceCode/Tests/src/kheap_test.c
@@ -0,0 +1,5 @@
+//
+// Created by XingfengYang on 2020/7/6.
+//
+
+#include <kheap_test.h>
