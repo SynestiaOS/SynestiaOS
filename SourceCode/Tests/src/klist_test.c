272888792ade2fe824b60b29c140b0b524ee645d
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-06 21:45:56 +0800

    [XingfengYang] #Test for klist

--- SourceCode/Tests/src/klist_test.c
+++ SourceCode/Tests/src/klist_test.c
@@ -5,15 +5,11 @@
 
 #include <klist_test.h>
 #include <tests_lib.h>
-#include <stdbool.h>
 #include <list.h>
 
 
 void should_klist_insert() {
     ListNode list;
-    ASSERT_EQ(list.next, nullptr)
-    ASSERT_EQ(list.prev, nullptr)
-
     ListNode newNode;
 
     klist_insert(&list, &newNode);
@@ -23,11 +19,42 @@ void should_klist_insert() {
 }
 
 void should_klist_remove_node() {
-    //todo:
-    ASSERT_TRUE(false)
+    ListNode list;
+    ListNode newNode;
+
+    klist_insert(&list, &newNode);
+
+    ASSERT_EQ(list.next, &newNode)
+    ASSERT_EQ(newNode.prev, &list)
+
+    ListNode newNode2;
+    klist_insert(&newNode, &newNode2);
+    ASSERT_EQ(newNode.next, &newNode2)
+    ASSERT_EQ(newNode2.prev, &newNode)
+
+    klist_remove_node(&newNode);
+    ASSERT_EQ(list.next, &newNode2)
+    ASSERT_EQ(newNode2.prev, &list)
 }
 
 void should_klist_return_size() {
-    //todo:
-    ASSERT_TRUE(false)
+    ListNode list;
+    ListNode newNode;
+
+    klist_insert(&list, &newNode);
+    ASSERT_EQ(klist_size(&list), 2)
+
+    ASSERT_EQ(list.next, &newNode)
+    ASSERT_EQ(newNode.prev, &list)
+
+    ListNode newNode2;
+    klist_insert(&newNode, &newNode2);
+    ASSERT_EQ(newNode.next, &newNode2)
+    ASSERT_EQ(newNode2.prev, &newNode)
+    ASSERT_EQ(klist_size(&newNode2), 3)
+
+    klist_remove_node(&newNode);
+    ASSERT_EQ(list.next, &newNode2)
+    ASSERT_EQ(newNode2.prev, &list)
+    ASSERT_EQ(klist_size(&list), 2)
 }
