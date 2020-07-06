8a33a6eb239f67d2cf4bdedf828489f6dd4fed9d
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-06 23:17:32 +0800

    [XingfengYang] #N/A

--- SourceCode/Tests/src/test_lib.c
+++ SourceCode/Tests/src/test_lib.c
@@ -7,11 +7,11 @@
 void __assert_func_failed(const char *file, int line, const char *func, const char *failedexpr) {
     __test_all_asserts++;
     __test_failed_asserts++;
-    if(__test_all_asserts==1){
+    if (__test_all_asserts > 1) {
         printf("├──\033[31mAssertion \"%s\" false \033[0m\n", failedexpr);
         printf("│    ├──\033[31mFile: %s, Line: %d \033[0m\n", file, line);
         printf("│    └──\033[31mFunction: %s \033[0m\n", func);
-    }else{
+    } else {
         printf("└──\033[31mAssertion \"%s\" false \033[0m\n", failedexpr);
         printf("     ├──\033[31mFile: %s, Line: %d \033[0m\n", file, line);
         printf("     └──\033[31mFunction: %s \033[0m\n", func);
@@ -20,5 +20,9 @@ void __assert_func_failed(const char *file, int line, const char *func, const ch
 
 void __assert_func_success(const char *file, int line, const char *func, const char *failedexpr) {
     __test_all_asserts++;
-    printf("└──\033[32mAssertion \"%s\" true \033[0m\n", failedexpr);
+    if (__test_all_asserts > 1) {
+        printf("├──\033[32mAssertion \"%s\" true \033[0m\n", failedexpr);
+    } else {
+        printf("└──\033[32mAssertion \"%s\" true \033[0m\n", failedexpr);
+    }
 }
