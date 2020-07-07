85c2453f89c1d298882b84ceb580056bb9ec474b
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-07 23:24:34 +0800

    [Xingfengyang] #GUI

---
+++ SourceCode/Libraries/LibGUI/include/gui_label.h
@@ -0,0 +1,8 @@
+//
+// Created by XingfengYang on 2020/7/7.
+//
+
+#ifndef __LIBRARY_LIBGUI_LABEL_H__
+#define __LIBRARY_LIBGUI_LABEL_H__
+
+#endif //__LIBRARY_LIBGUI_LABEL_H__
---
+++ SourceCode/Libraries/LibGUI/src/gui_button.c
@@ -0,0 +1,4 @@
+//
+// Created by XingfengYang on 2020/7/7.
+//
+#include <gui_button.h>
