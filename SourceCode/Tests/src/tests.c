423cf4acb4f4c77f46ce8c9f82222db87ec5ef49
shifu <wooshifu@outlook.com>
2020-07-08 20:35:58 +0800

    fix kernel_main redefinition issue

--- SourceCode/Tests/src/tests.c
+++ SourceCode/Tests/src/tests.c
@@ -11,21 +11,21 @@
 void kernel_main_tests() {
     kheap_init();
 
-    TEST_SUIT("should_klist_insert", should_klist_insert)
-    TEST_SUIT("should_klist_append", should_klist_append)
-    TEST_SUIT("should_klist_remove_node", should_klist_remove_node)
-    TEST_SUIT("should_klist_return_size", should_klist_return_size)
+    TEST_CASE("should_klist_insert", should_klist_insert);
+    TEST_CASE("should_klist_append", should_klist_append);
+    TEST_CASE("should_klist_remove_node", should_klist_remove_node);
+    TEST_CASE("should_klist_return_size", should_klist_return_size);
 
-    TEST_SUIT("should_kstack_create", should_kstack_create)
-    TEST_SUIT("should_kstack_push_value", should_kstack_push_value)
-    TEST_SUIT("should_kstack_pop_value", should_kstack_pop_value)
-    TEST_SUIT("should_kstack_peek_value", should_kstack_peek_value)
-    TEST_SUIT("should_kstack_return_true_when_empty", should_kstack_return_true_when_empty)
-    TEST_SUIT("should_kstack_return_true_when_full", should_kstack_return_true_when_full)
+    TEST_CASE("should_kstack_create", should_kstack_create);
+    TEST_CASE("should_kstack_push_value", should_kstack_push_value);
+    TEST_CASE("should_kstack_pop_value", should_kstack_pop_value);
+    TEST_CASE("should_kstack_peek_value", should_kstack_peek_value);
+    TEST_CASE("should_kstack_return_true_when_empty", should_kstack_return_true_when_empty);
+    TEST_CASE("should_kstack_return_true_when_full", should_kstack_return_true_when_full);
 
-    TEST_SUIT("should_kheap_init", should_kheap_init)
-    TEST_SUIT("should_kheap_alloc", should_kheap_alloc)
-    TEST_SUIT("should_kheap_calloc", should_kheap_calloc)
-    TEST_SUIT("should_kheap_realloc", should_kheap_realloc)
-    TEST_SUIT("should_kheap_free", should_kheap_free)
+    TEST_CASE("should_kheap_init", should_kheap_init);
+    TEST_CASE("should_kheap_alloc", should_kheap_alloc);
+    TEST_CASE("should_kheap_calloc", should_kheap_calloc);
+    TEST_CASE("should_kheap_realloc", should_kheap_realloc);
+    TEST_CASE("should_kheap_free", should_kheap_free);
 }
