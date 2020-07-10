72aa3cf981d78e3a6c9a81c2b42b60d79348e145
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-10 17:44:09 +0800

    [XingfengYang] Vector

--- SourceCode/Kernel/src/kvector.c
+++ SourceCode/Kernel/src/kvector.c
@@ -2,10 +2,21 @@
 // Created by XingfengYang on 2020/7/10.
 //
 #include <kvector.h>
+#include <kheap.h>
+#include <stdlib.h>
 
 
 KernelVector *kvector_allocate() {
-
+    // 1. allocate vector memory block from virtual memory (heap), and align.
+    KernelVector *vector = (KernelVector *) kheap_alloc(DEFAULT_VECTOR_SIZE * sizeof(ListNode) + sizeof(KernelVector));
+    if (vector == nullptr) {
+        printf("[KVector] kVector allocate failed.\n");
+        return nullptr;
+    }
+    vector->size = DEFAULT_VECTOR_SIZE;
+    vector->index = 0;
+    vector->node = (ListNode *) (vector + sizeof(KernelVector));
+    return vector;
 }
 
 KernelStatus kvector_resize(KernelVector *vector, uint32_t newSize) {
