175fdd8ce648496b3ab42981a394843645a1fc8b
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-10 17:40:48 +0800

    [XingfengYang] Vector

---
+++ SourceCode/Kernel/src/kvector.c
@@ -0,0 +1,45 @@
+//
+// Created by XingfengYang on 2020/7/10.
+//
+#include <kvector.h>
+
+
+KernelVector *kvector_allocate() {
+
+}
+
+KernelStatus kvector_resize(KernelVector *vector, uint32_t newSize) {
+
+}
+
+KernelStatus kvector_free(KernelVector *vector) {
+
+}
+
+KernelStatus kvector_add(KernelVector *vector, ListNode *node) {
+
+}
+
+KernelStatus kvector_get(KernelVector *vector, uint32_t index) {
+
+}
+
+KernelStatus kvector_remove_index(KernelVector *vector, uint32_t index) {
+
+}
+
+KernelStatus kvector_remove(KernelVector *vector, ListNode *index) {
+
+}
+
+bool kvector_is_empty(KernelVector *vector) {
+
+}
+
+bool kvector_is_full(KernelVector *vector) {
+
+}
+
+KernelStatus kvector_clear(KernelVector *vector) {
+
+}
