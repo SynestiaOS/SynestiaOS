fffa57f22725a1bc109eff049ed496843f2bf864
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-13 14:44:05 +0800

    [Xingfengyang] SysCall: prorotype

--- SourceCode/Kernel/include/sys_call.h
+++ SourceCode/Kernel/include/sys_call.h
@@ -5,10 +5,8 @@
 #ifndef __KERNEL_SYSCALL_H__
 #define __KERNEL_SYSCALL_H__
 
-#define SYSCALL_DEFINE0(name)   \
-    asmlinkage long sys_##name##(void){ \
-            \
-    }       \
+#define SYSCALL_DEFINE0(name) asmlinkage long sys_##name##(void);
 
+// SYSCALL_DEFINE0(get_pid)
 
 #endif // __KERNEL_SYSCALL_H__
