5e2b9d4bdecd5a7f9d501455d345165899761fb2
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-22 11:34:19 +0800

    [Xingfengyang] #CSF: add to csf schduler tree

--- SourceCode/Kernel/src/rbtree.c
+++ SourceCode/Kernel/src/rbtree.c
@@ -1,11 +1,17 @@
 //
 // Created by XingfengYang on 2020/6/30.
 //
-
 #include <kheap.h>
 #include <rbtree.h>
 #include <log.h>
 
+/**
+ * 1. Node is red or black
+ * 2. Root node is black
+ * 3. All leaf nodes are nullptr and black
+ * 4. The red node's children must be black, it means there can not have two connected red node from root to a leaf node
+ * 5. All paths from any node to each of its leaves contain the same number of black nodes
+ **/
 void rbtree_balance(RBNode *root, RBNode *node) {
   LogWarnning("[CSF] balance.\n");
 }
