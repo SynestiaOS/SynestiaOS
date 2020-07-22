45f03db22390d0b618c0966ded9962707d67ab92
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-22 10:00:21 +0800

    [Xingfengyang] #Barrier: define

---
+++ SourceCode/Kernel/include/barrier.h
@@ -0,0 +1,17 @@
+//
+// Created by XingfengYang on 2020/7/22.
+//
+
+#ifndef __KERNEL_BARRIER_H__
+#define __KERNEL_BARRIER_H__
+
+#include <mutex.h>
+
+typedef struct Barrier{
+    uint32_t arrivalCounter;
+    uint32_t leaveCounter;
+    uint32_t flag;
+    Mutex lock;
+};
+
+#endif // __KERNEL_BARRIER_H__
---
+++ SourceCode/Kernel/src/barrier.c
@@ -0,0 +1,4 @@
+//
+// Created by XingfengYang on 2020/7/22.
+//
+#include <barrier.h>
