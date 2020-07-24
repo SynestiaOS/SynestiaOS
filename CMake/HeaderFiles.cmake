2bacff1f05523da48312b31f7b050598d679a038
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-24 15:07:01 +0800

    [Xingfengyang] #N/A add raspi4

--- CMake/HeaderFiles.cmake
+++ CMake/HeaderFiles.cmake
@@ -18,6 +18,10 @@ function(target_include_raspberry_pi3_header_files Target)
     target_include_directories(${Target} PRIVATE ${SYNESTIAOS_SOURCE_CODE_DIR}/Board/RaspberryPi3/include)
 endfunction()
 
+function(target_include_raspberry_pi4_header_files Target)
+    target_include_directories(${Target} PRIVATE ${SYNESTIAOS_SOURCE_CODE_DIR}/Board/RaspberryPi4/include)
+endfunction()
+
 function(target_include_kernel_header_files Target)
     target_include_directories(${Target} PRIVATE ${SYNESTIAOS_SOURCE_CODE_DIR}/Kernel/include)
 endfunction()
