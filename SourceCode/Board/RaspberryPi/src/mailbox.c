9bf5ae4354b6e4ada75593475204dd8a778b668d
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-08-03 16:57:22 +0800

    [Xingfengyang] #.elf

--- SourceCode/Board/RaspberryPi/src/mailbox.c
+++ SourceCode/Board/RaspberryPi/src/mailbox.c
@@ -29,25 +29,20 @@ uint32_t mailbox_call(uint8_t channel, uint32_t mailbox) {
 
 uint32_t mailbox_read(uint8_t channel) {
   // Loop until we receive something from the requested channel
-  for (;;) {
-    while ((*(uint32_t *)(MAIL0_STATUS)&MBOX_EMPTY) != 0) {
+  uint32_t data;
+  do {
+    while (*(uint32_t *)(MAIL0_STATUS) & MBOX_EMPTY) {
       // Wait for data
     }
-    // Read the data
-    uint32_t data = *(uint32_t *)(MAIL0_READ);
-    uint32_t readChannel = data & 0xF;
-    data >>= 4;
-    // Return it straight away if it's for the requested channel
-    if (readChannel == channel) {
-      return data;
-    }
-  }
+    data = *(uint32_t *)(MAIL0_READ);
+  } while ((data & 0xF) != channel);	
+  return data & ~0xF;
 }
 
 void mailbox_write(uint8_t channel, uint32_t data) {
-  while ((*(uint32_t *)(MAIL0_STATUS)&MBOX_FULL) != 0) {
+  while (*(uint32_t *)(MAIL0_STATUS)&MBOX_FULL) {
     // Wait for data
   }
   // Write the value to the requested channel
-  *(uint32_t *)(MAIL0_WRITE) = ((data & ~0xF) | channel);
+  *(uint32_t *)(MAIL0_WRITE) = (data | channel);
 }
