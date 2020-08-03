1edde19d810501c361df0ddb8d33cfbb08a0c265
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-08-03 20:34:11 +0800

    [Xingfengyang] #N/A

--- SourceCode/Board/RaspberryPi/include/framebuffer.h
+++ SourceCode/Board/RaspberryPi/include/framebuffer.h
@@ -7,21 +7,6 @@
 
 #include <stdint.h>
 
-#define COLORDEPTH 24
-#define BYTES_PER_PIXEL COLORDEPTH / 8
-
-typedef struct psf_t {
-  uint32_t magic;
-  uint32_t version;
-  uint32_t headersize;
-  uint32_t flags;
-  uint32_t numglyph;
-  uint32_t bytesperglyph;
-  uint32_t height;
-  uint32_t width;
-  unsigned char glyphs;
-} __attribute__((packed)) psf_t;
-
 int framebuffer_init(void);
 
 void framebuffer_draw_pixel(uint32_t x, uint32_t y, char r, char g, char b);
