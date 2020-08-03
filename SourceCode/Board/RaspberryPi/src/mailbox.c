374c610d130b7a6c6f31bf7bb40b28241fc3bbef
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-08-03 17:00:19 +0800

    [Xingfengyang] #N/A

--- SourceCode/Board/RaspberryPi/src/mailbox.c
+++ SourceCode/Board/RaspberryPi/src/mailbox.c
@@ -6,21 +6,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-struct mbox_registers {
-  const volatile uint32_t read_0; // 0x00         Read data from VC to ARM
-  uint32_t unused[3];             // 0x04-0x0F
-  volatile uint32_t peek_0;       // 0x10
-  volatile uint32_t sender_0;     // 0x14
-  volatile uint32_t status_0;     // 0x18         Status of VC to ARM
-  volatile uint32_t config0;      // 0x1C
-  volatile uint32_t write_1;      // 0x20         Write data from ARM to VC
-  uint32_t unused_2[3];           // 0x24-0x2F
-  volatile uint32_t peek_1;       // 0x30
-  volatile uint32_t sender_1;     // 0x34
-  volatile uint32_t status_1;     // 0x38         Status of ARM to VC
-  volatile uint32_t config_1;     // 0x3C
-} __attribute__((__packed__, aligned(4)));
-
 uint32_t mailbox_call(uint8_t channel, uint32_t mailbox) {
   while (!(*(uint32_t *)(MAIL0_STATUS) & MBOX_EMPTY)){
 		*(uint32_t *)(MAIL0_READ);
