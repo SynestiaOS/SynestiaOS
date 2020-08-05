dc0f9ef75c1387f8a0d741d56d35b2a0fe1549f6
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-08-05 14:32:34 +0800

    [XingfengYang] #Schd

--- SourceCode/Kernel/src/percpu.c
+++ SourceCode/Kernel/src/percpu.c
@@ -3,76 +3,77 @@
 //
 
 #include <kheap.h>
+#include <log.h>
 #include <percpu.h>
 #include <thread.h>
-#include <log.h>
 
 PerCpu *perCpu = nullptr;
 
 KernelStatus percpu_default_insert_thread(PerCpu *perCpu, Thread *thread) {
-    LogWarn("[PerCpu]: insert thread '%s' to cpu '%d'.\n", thread->name,perCpu->cpuId);
-    perCpu->rbTree.operations.insert(&perCpu->rbTree, &thread->rbNode);
-    perCpu->priority += thread->priority;
-    return OK;
+  LogWarn("[PerCpu]: insert thread '%s' to cpu '%d'.\n", thread->name, perCpu->cpuId);
+  perCpu->rbTree.operations.insert(&perCpu->rbTree, &thread->rbNode);
+  perCpu->priority += thread->priority;
+  return OK;
 }
 
 Thread *percpu_default_remove_thread(PerCpu *perCpu, Thread *thread) {
-    LogWarn("[PerCpu]: remove thread '%s' from cpu '%d'.\n", thread->name,perCpu->cpuId);
-    RBNode *node = perCpu->rbTree.operations.remove(&perCpu->rbTree, &thread->rbNode);
-    perCpu->priority -= thread->priority;
-    return node;
+  LogWarn("[PerCpu]: remove thread '%s' from cpu '%d'.\n", thread->name, perCpu->cpuId);
+  perCpu->rbTree.operations.remove(&perCpu->rbTree, &thread->rbNode);
+  perCpu->priority -= thread->priority;
+  return thread;
 }
 
 Thread *percpu_default_get_next_thread(PerCpu *perCpu) {
-    PerCpu *min = percpu_min_priority();
-    RBNode *node = min->rbTree.operations.getMin(&min->rbTree);
-    if (node == nullptr) {
-        // todo: migration from other core.
-        LogWarn("[PerCpu]: get min thread null.\n");
-        return perCpu->idleThread;
-    }
-    Thread *thread = getNode(node, Thread, rbNode);
-    if (thread == nullptr) {
-        return perCpu->idleThread;
-    }
-    return thread;
+  PerCpu *min = percpu_min_priority();
+  RBNode *node = min->rbTree.operations.getMin(&min->rbTree);
+  if (node == nullptr) {
+    // todo: migration from other core.
+    LogWarn("[PerCpu]: get min thread null.\n");
+    return perCpu->idleThread;
+  }
+  Thread *thread = getNode(node, Thread, rbNode);
+  if (thread == nullptr) {
+    return perCpu->idleThread;
+  }
+  return thread;
 }
 
 KernelStatus percpu_default_init(PerCpu *perCpu, uint32_t num, Thread *idleThread) {
-    perCpu->idleThread = idleThread;
-    perCpu->cpuId = num;
-    perCpu->priority = 0;
-    perCpu->status.idleTime = 0;
-    perCpu->currentThread = nullptr;
-    perCpu->rbTree.root = nullptr;
-    rb_tree_init(&perCpu->rbTree);
-    perCpu->waitThreadQueue.next = nullptr;
-    perCpu->waitThreadQueue.prev = nullptr;
-    return OK;
+  perCpu->idleThread = idleThread;
+  perCpu->cpuId = num;
+  perCpu->priority = 0;
+  perCpu->status.idleTime = 0;
+  perCpu->currentThread = nullptr;
+  perCpu->rbTree.root = nullptr;
+  rb_tree_init(&perCpu->rbTree);
+  perCpu->waitThreadQueue.next = nullptr;
+  perCpu->waitThreadQueue.prev = nullptr;
+  LogInfo("[PerCpu]: precpu '%d' inited.\n", num);
+  return OK;
 }
 
 KernelStatus percpu_create(uint32_t cpuNum) {
-    perCpu = (PerCpu *) kheap_calloc(cpuNum, sizeof(PerCpu));
-    if (perCpu == nullptr) {
-        return ERROR;
-    }
-    for (uint32_t cpuId = 0; cpuId < cpuNum; cpuId++) {
-        perCpu[cpuId].operations.init = percpu_default_init;
-        perCpu[cpuId].operations.insertThread = percpu_default_insert_thread;
-        perCpu[cpuId].operations.removeThread = percpu_default_remove_thread;
-        perCpu[cpuId].operations.getNextThread = percpu_default_get_next_thread;
-    }
-    return OK;
+  perCpu = (PerCpu *)kheap_calloc(cpuNum, sizeof(PerCpu));
+  if (perCpu == nullptr) {
+    return ERROR;
+  }
+  for (uint32_t cpuId = 0; cpuId < cpuNum; cpuId++) {
+    perCpu[cpuId].operations.init = percpu_default_init;
+    perCpu[cpuId].operations.insertThread = percpu_default_insert_thread;
+    perCpu[cpuId].operations.removeThread = percpu_default_remove_thread;
+    perCpu[cpuId].operations.getNextThread = percpu_default_get_next_thread;
+  }
+  return OK;
 }
 
 PerCpu *percpu_get(CpuNum cpuNum) { return &perCpu[cpuNum]; }
 
 PerCpu *percpu_min_priority() {
-    PerCpu *min = &perCpu[0];
-    for (uint32_t cpuId = 0; cpuId < CPU_EXISTS_NUM; cpuId++) {
-        if (perCpu[cpuId].priority < min->priority) {
-            min = &perCpu[cpuId];
-        }
+  PerCpu *min = &perCpu[0];
+  for (uint32_t cpuId = 0; cpuId < CPU_EXISTS_NUM; cpuId++) {
+    if (perCpu[cpuId].priority < min->priority) {
+      min = &perCpu[cpuId];
     }
-    return min;
+  }
+  return min;
 }
--- SourceCode/Kernel/src/rbtree.c
+++ SourceCode/Kernel/src/rbtree.c
@@ -41,15 +41,17 @@ RBNode *rbtree_insert(RBNode *node1, RBNode *node2) {
       node1->left = node2;
       node2->parent = node1;
       return node2;
+    } else {
+      return rbtree_insert(node1->left, node2);
     }
-    return rbtree_insert(node1->left, node2);
   } else {
     if (node1->right == nullptr) {
       node1->right = node2;
       node2->parent = node1;
       return node2;
+    } else {
+      return rbtree_insert(node1->right, node2);
     }
-    return rbtree_insert(node1->right, node2);
   }
 }
 
@@ -59,9 +61,8 @@ RBNode *rbtree_default_insert(RBTree *tree, RBNode *node) {
   if (root == nullptr) {
     tree->root = node;
     return node;
-  } else {
-    return rbtree_insert(root, node);
   }
+  return rbtree_insert(root, node);
 }
 
 RBNode *rbtree_default_remove(RBTree *tree, RBNode *node) {
@@ -73,11 +74,13 @@ RBNode *rbtree_default_remove(RBTree *tree, RBNode *node) {
     root->right = nullptr;
   } else {
     if (node->parent->left == node) {
-      node->parent->left == nullptr;
+      node->parent->left = nullptr;
     } else if (node->parent->right == node) {
-      node->parent->right == nullptr;
+      node->parent->right = nullptr;
     }
   }
+
+  node->parent = nullptr;
   if (left != nullptr) {
     rbtree_default_insert(tree, left);
   }
