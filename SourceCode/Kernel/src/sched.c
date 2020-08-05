7f067faa899d2a5fa66251fd6617035f999087d4
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-08-05 16:44:00 +0800

    [Xingfengyang] #N/A

--- SourceCode/Kernel/src/sched.c
+++ SourceCode/Kernel/src/sched.c
@@ -123,4 +123,8 @@ KernelStatus schd_preempt(void) {
   return OK;
 }
 
-uint32_t schd_getpid() { return currentThread->pid; }
+uint32_t schd_getpid() { 
+  uint32_t cpuid = read_cpuid();
+  PerCpu *perCpu = percpu_get(cpuid);
+  return perCpu->currentThread->pid; 
+}
