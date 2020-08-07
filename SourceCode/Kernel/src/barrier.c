b2c8de5fc3d43276b405e526f8ee93516fca06e1
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-08-08 00:17:33 +0800

    [XingfengYang] #N/A

--- SourceCode/Kernel/src/barrier.c
+++
@@ -1,4 +0,0 @@
-//
-// Created by XingfengYang on 2020/7/22.
-//
-#include <barrier.h>
--- SourceCode/Libraries/LibC/src/stdlib.c
+++ SourceCode/Libraries/LibC/src/stdlib.c
@@ -1,8 +1,6 @@
 #include <uart.h>
-
 #include <stdint.h>
 #include <stdlib.h>
-
 #include <stdarg.h>
 #include <string.h>
 
