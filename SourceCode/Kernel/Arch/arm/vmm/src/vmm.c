f0d1cd235508ef51411a32557f4b67c638de93f4
Xingfeng Yang <nerosoft@outlook.com>
2020-09-07 00:52:39 +0800

    [XingfengYang] #VMM: refactor map process

--- SourceCode/Kernel/Arch/arm/vmm/src/vmm.c
+++ SourceCode/Kernel/Arch/arm/vmm/src/vmm.c
@@ -11,7 +11,72 @@
 #include <vmm.h>
 
 void virtual_memory_default_allocate_page(VirtualMemory *virtualMemory, uint32_t virtualAddress) {
-  // TODO:
+  uint32_t l1Offset = virtualAddress >> 30 & 0b11;
+  uint32_t l2Offset = virtualAddress >> 21 & 0b111111111;
+  uint32_t l3Offset = virtualAddress >> 12 & 0b111111111;
+  uint32_t pageOffset = virtualAddress & 0xFFF;
+  PageTableEntry *level1PageTable = virtualMemory->pageTable;
+  PageTableEntry level1PageTableEntry = level1PageTable[l1Offset];
+  if (level1PageTableEntry.valid == 0) {
+    // level 1 page table entry not set.
+    uint64_t l2ptPage = virtualMemory->physicalPageAllocator->operations.allocPage4K(
+        virtualMemory->physicalPageAllocator, USAGE_PAGE_TABLE);
+    PageTableEntry *l2pt = (PageTableEntry *)virtualMemory->physicalPageAllocator->base + l2ptPage * PAGE_SIZE;
+
+    uint64_t ptPage = virtualMemory->physicalPageAllocator->operations.allocPage4K(virtualMemory->physicalPageAllocator,
+                                                                                   USAGE_PAGE_TABLE);
+    PageTableEntry *pt = (PageTableEntry *)virtualMemory->physicalPageAllocator->base + ptPage * PAGE_SIZE;
+
+    pt[0].valid = 1;
+    pt[0].table = 1;
+    pt[0].af = 1;
+    pt[0].base = (uint64_t)(virtualMemory->physicalPageAllocator->operations.allocPage4K(
+        virtualMemory->physicalPageAllocator, USAGE_NORMAL));
+
+    l2pt[0].valid = 1;
+    l2pt[0].table = 1;
+    l2pt[0].af = 1;
+    l2pt[0].base = ptPage;
+
+    level1PageTableEntry.valid = 1;
+    level1PageTableEntry.table = 1;
+    level1PageTableEntry.af = 1;
+    level1PageTableEntry.base = l2ptPage;
+  } else {
+    PageTableEntry *level2PageTable = (PageTableEntry *)(level1PageTableEntry.base >> VA_OFFSET);
+    PageTableEntry level2PageTableEntry = level2PageTable[l2Offset];
+    if (level2PageTableEntry.valid == 0) {
+      //   level 2 page table entry not set.
+      uint64_t ptPage = virtualMemory->physicalPageAllocator->operations.allocPage4K(
+          virtualMemory->physicalPageAllocator, USAGE_PAGE_TABLE);
+      PageTableEntry *pt = (PageTableEntry *)virtualMemory->physicalPageAllocator->base + ptPage * PAGE_SIZE;
+
+      pt[0].valid = 1;
+      pt[0].table = 1;
+      pt[0].af = 1;
+      pt[0].base = (uint64_t)(virtualMemory->physicalPageAllocator->operations.allocPage4K(
+          virtualMemory->physicalPageAllocator, USAGE_NORMAL));
+
+      level2PageTableEntry.valid = 1;
+      level2PageTableEntry.table = 1;
+      level2PageTableEntry.af = 1;
+      level2PageTableEntry.base = ptPage;
+    } else {
+      PageTableEntry *pageTable = (PageTableEntry *)(level2PageTable->base >> VA_OFFSET);
+      PageTableEntry pageTableEntry = pageTable[l3Offset];
+      if (pageTableEntry.valid == 0) {
+        // page table entry not set
+        pageTableEntry.valid = 1;
+        pageTableEntry.table = 1;
+        pageTableEntry.af = 1;
+        pageTableEntry.base = (uint64_t)(virtualMemory->physicalPageAllocator->operations.allocPage4K(
+            virtualMemory->physicalPageAllocator, USAGE_NORMAL));
+
+      } else {
+        // should not be there, if goto there, means it not a page fault exception
+      }
+    }
+  }
 }
 
 void virtual_memory_default_mapping_page(VirtualMemory *virtualMemory, uint32_t virtualAddress,
@@ -113,92 +178,23 @@ KernelStatus vmm_create(VirtualMemory *virtualMemory, PhysicalPageAllocator *phy
 
 void do_page_fault(uint32_t address) {
   LogError("[vmm]: page fault at: %d .\n", address);
-
   uint32_t l1Offset = address >> 30 & 0b11;
   uint32_t l2Offset = address >> 21 & 0b111111111;
   uint32_t l3Offset = address >> 12 & 0b111111111;
   uint32_t pageOffset = address & 0xFFF;
+  LogError("[vmm]: l1Offset: %d .\n", l1Offset);
+  LogError("[vmm]: l2Offset: %d .\n", l2Offset);
+  LogError("[vmm]: l3Offset: %d .\n", l3Offset);
+  LogError("[vmm]: pageOffset: %d .\n", pageOffset);
 
   // check is there is a thread running, if it was, then map for thread's vmm:
   // TODO: it not good, may be make some mistake when thread is running and kernel triggered this.
   Thread *currThread = schd_get_current_thread();
   if (currThread != nullptr) {
     // may be user triggered this
-
     VirtualMemory virtualMemory = currThread->memoryStruct.virtualMemory;
-    PageTableEntry *level1PageTable = virtualMemory.pageTable;
-    PageTableEntry level1PageTableEntry = level1PageTable[l1Offset];
-    if (level1PageTableEntry.valid == 0) {
-      // level 1 page table entry not set.
-      uint64_t l2ptPage = virtualMemory.physicalPageAllocator->operations.allocPage4K(
-          virtualMemory.physicalPageAllocator, USAGE_PAGE_TABLE);
-      PageTableEntry *l2pt = (PageTableEntry *)virtualMemory.physicalPageAllocator->base + l2ptPage * PAGE_SIZE;
-
-      uint64_t ptPage = virtualMemory.physicalPageAllocator->operations.allocPage4K(virtualMemory.physicalPageAllocator,
-                                                                                    USAGE_PAGE_TABLE);
-      PageTableEntry *pt = (PageTableEntry *)virtualMemory.physicalPageAllocator->base + ptPage * PAGE_SIZE;
-
-      pt[0].valid = 1;
-      pt[0].table = 1;
-      pt[0].af = 1;
-      pt[0].base = (uint64_t)(virtualMemory.physicalPageAllocator->operations.allocPage4K(
-          virtualMemory.physicalPageAllocator, USAGE_NORMAL));
-
-      l2pt[0].valid = 1;
-      l2pt[0].table = 1;
-      l2pt[0].af = 1;
-      l2pt[0].base = ptPage;
-
-      level1PageTableEntry.valid = 1;
-      level1PageTableEntry.table = 1;
-      level1PageTableEntry.af = 1;
-      level1PageTableEntry.base = l2ptPage;
-    } else {
-      PageTableEntry *level2PageTable = (PageTableEntry *)(level1PageTableEntry.base >> VA_OFFSET);
-      PageTableEntry level2PageTableEntry = level2PageTable[l2Offset];
-      if (level2PageTableEntry.valid == 0) {
-        //   level 2 page table entry not set.
-        uint64_t ptPage = virtualMemory.physicalPageAllocator->operations.allocPage4K(
-            virtualMemory.physicalPageAllocator, USAGE_PAGE_TABLE);
-        PageTableEntry *pt = (PageTableEntry *)virtualMemory.physicalPageAllocator->base + ptPage * PAGE_SIZE;
-
-        pt[0].valid = 1;
-        pt[0].table = 1;
-        pt[0].af = 1;
-        pt[0].base = (uint64_t)(virtualMemory.physicalPageAllocator->operations.allocPage4K(
-            virtualMemory.physicalPageAllocator, USAGE_NORMAL));
-
-        level2PageTableEntry.valid = 1;
-        level2PageTableEntry.table = 1;
-        level2PageTableEntry.af = 1;
-        level2PageTableEntry.base = ptPage;
-      } else {
-        PageTableEntry *pageTable = (PageTableEntry *)(level2PageTable->base >> VA_OFFSET);
-        PageTableEntry pageTableEntry = pageTable[l3Offset];
-        if (pageTableEntry.valid == 0) {
-          // page table entry not set
-          pageTableEntry.valid = 1;
-          pageTableEntry.table = 1;
-          pageTableEntry.af = 1;
-          pageTableEntry.base = (uint64_t)(virtualMemory.physicalPageAllocator->operations.allocPage4K(
-              virtualMemory.physicalPageAllocator, USAGE_NORMAL));
-
-        } else {
-          // should not be there, if goto there, means it not a page fault exception
-        }
-      }
-    }
+    virtualMemory.operations.allocatePage(&virtualMemory, address);
   } else {
     // kernel triggered this
   }
-
-  // or user address
-
-  // check is thread not map vm
-  LogError("[vmm]: l1Offset: %d .\n", l1Offset);
-  LogError("[vmm]: l2Offset: %d .\n", l2Offset);
-  LogError("[vmm]: l3Offset: %d .\n", l3Offset);
-  LogError("[vmm]: pageOffset: %d .\n", pageOffset);
-
-  // alloc physical for page table and map the address
 }
