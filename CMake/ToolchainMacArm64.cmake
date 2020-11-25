2be7beaad185df74173b8d11a73ab521190b4eea
shifu <shifu.wu@outlook.com>
2020-11-25 19:46:35 +0800

    [shifu] #compiler use aarch64-unknown-linux-gnu-gcc, version 8.3.0

--- CMake/ToolchainMacArm64.cmake
+++ CMake/ToolchainMacArm64.cmake
@@ -1,25 +1,42 @@
+message(STATUS "using aarch64-unknown-linux-gnu-gcc, version 8.3.0")
+
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-find_program(MacAarch64 aarch64-none-elf-gcc REQUIRED)
-if (NOT MacAarch64)
-    message(FATAL_ERROR "aarch64-none-elf-gcc not found!!!\n"
-            "run `brew tap SergioBenitez/osxct && brew install aarch64-none-elf` to install the toolchain")
-else ()
-    message(STATUS "found aarch64-none-elf-gcc ${MacAarch64}")
+set(arm_gcc_toolchain_file_name aarch64-unknown-linux-gnu.tar.xz)
+set(arm_gcc_toolchain_path $ENV{HOME}/${arm_gcc_toolchain_file_name})
+set(arm_gcc_toolchain_dir $ENV{HOME}/aarch64-unknown-linux-gnu)
+set(arm_gcc_toolchain_url https://github.com/thinkski/osx-arm-linux-toolchains/releases/download/8.3.0/aarch64-unknown-linux-gnu.tar.xz)
+
+if (NOT EXISTS ${arm_gcc_toolchain_dir})
+    if (NOT EXISTS ${arm_gcc_toolchain_path})
+        message(STATUS "downloading ${arm_gcc_toolchain_url}, wait a moment")
+        file(DOWNLOAD ${arm_gcc_toolchain_url} ${arm_gcc_toolchain_path}
+                EXPECTED_MD5 a8acb994a78185191e6be31f3573cec0
+                TLS_VERIFY ON
+                SHOW_PROGRESS)
+    endif ()
+
+    file(MAKE_DIRECTORY ${arm_gcc_toolchain_dir})
+    message(STATUS "extracting ${arm_gcc_toolchain_path}, wait a moment")
+    execute_process(COMMAND
+            ${CMAKE_COMMAND} -E tar xvzf ${arm_gcc_toolchain_path}
+            WORKING_DIRECTORY $ENV{HOME})
 endif ()
 
-set(CMAKE_C_COMPILER aarch64-none-elf-gcc)
-set(CMAKE_CXX_COMPILER aarch64-none-elf-g++)
+
+set(CMAKE_FIND_ROOT_PATH ${arm_gcc_toolchain_dir}/bin)
+
+set(CMAKE_C_COMPILER ${CMAKE_FIND_ROOT_PATH}/aarch64-unknown-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${CMAKE_FIND_ROOT_PATH}/aarch64-unknown-linux-gnu-g++)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR qemu-system-aarch64)
 
-set(CMAKE_FIND_ROOT_PATH /usr/local/bin)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-set(TOOLCHAIN_PREFIX aarch64-none-elf-)
-set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
+set(TOOLCHAIN_PREFIX aarch64-unknown-linux-gnu-)
+set(CMAKE_OBJCOPY ${arm_gcc_toolchain_dir}/bin/${TOOLCHAIN_PREFIX}objcopy)
