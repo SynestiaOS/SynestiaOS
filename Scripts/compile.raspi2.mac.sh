8683c669ebc25898bd8d2a03f6df3e4e158d30cf
shifu <shifu.wu@outlook.com>
2020-11-25 20:31:22 +0800

    [shifu] #compile add compile scripts

---
+++ Scripts/compile.raspi2.mac.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(dirname "$0")
+BUILD_DIR="${BASEDIR}"/buildRaspi2
+
+rm -rf "${BUILD_DIR}"
+mkdir "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
+cmake -DCMAKE_TOOLCHAIN_FILE="${BASEDIR}"/../../CMake/ToolchainMacArm.cmake -DARCH=arm -DPLATFORM=pi2 "${BASEDIR}/../.."
+VERBOSE=1 make
---
+++ Scripts/compile.raspi3.linux.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(dirname "$0")
+BUILD_DIR="${BASEDIR}"/buildRaspi3
+
+rm -rf "${BUILD_DIR}"
+mkdir "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit
+cmake -DCMAKE_TOOLCHAIN_FILE="${BASEDIR}"/../../CMake/ToolchainLinuxArm64.cmake -DARCH=arm64 -DPLATFORM=pi3 "${BASEDIR}/../.."
+VERBOSE=1 make
