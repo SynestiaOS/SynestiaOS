167d36d2af82015f6566d7818c9f2b6f1468d570
NeoYang <nerosoft@outlook.com>
2020-11-30 19:40:48 +0800

    [XingfengYang] #Kobject: add kobject to FileDdescriptor

--- SourceCode/Kernel/include/kernel/thread.h
+++ SourceCode/Kernel/include/kernel/thread.h
@@ -102,6 +102,7 @@ typedef struct FileDescriptor {
     uint32_t pos;
     DirectoryEntry *directoryEntry;
     ListNode node;
+    KernelObject object;
 } FileDescriptor;
 
 typedef uint32_t (*FilesStructOperationOpenFile)(struct FilesStruct *filesStruct,
