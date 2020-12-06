f1eb48e5329d859f90cdf371fda8743a89a96e41
NeoYang <nerosoft@outlook.com>
2020-12-06 19:45:01 +0800

    [XingfengYang] #MMU: no nee to set the domain access control register

--- SourceCode/Arch/arm/src/kernel_vmm.c
+++ SourceCode/Arch/arm/src/kernel_vmm.c
@@ -19,22 +19,22 @@ void map_kernel_pt(uint64_t pageTablePhysicalAddress) {
     kernelVMML1PT->pte[0].valid = 1;
     kernelVMML1PT->pte[0].table = 0;
     kernelVMML1PT->pte[0].af = 1;
-    kernelVMML1PT->pte[0].base = (uint32_t) 0 >> VA_OFFSET;
+    kernelVMML1PT->pte[0].base = (uint32_t) 0 >> 30;
 
     kernelVMML1PT->pte[1].valid = 1;
     kernelVMML1PT->pte[1].table = 0;
     kernelVMML1PT->pte[1].af = 1;
-    kernelVMML1PT->pte[1].base = (uint32_t)((1*GB) >> VA_OFFSET);
+    kernelVMML1PT->pte[1].base = (uint32_t)((1*GB) >> 30);
 
     kernelVMML1PT->pte[2].valid = 1;
     kernelVMML1PT->pte[2].table = 0;
     kernelVMML1PT->pte[2].af = 1;
-    kernelVMML1PT->pte[2].base = (uint32_t)((2*GB) >> VA_OFFSET);
+    kernelVMML1PT->pte[2].base = (uint32_t)((2*GB) >> 30);
 
     kernelVMML1PT->pte[3].valid = 1;
     kernelVMML1PT->pte[3].table = 0;
     kernelVMML1PT->pte[3].af = 1;
-    kernelVMML1PT->pte[3].base = (uint32_t)((3*GB) >> VA_OFFSET);
+    kernelVMML1PT->pte[3].base = (uint32_t)((3*GB) >> 30);
 }
 
 void map_kernel_mm() {
