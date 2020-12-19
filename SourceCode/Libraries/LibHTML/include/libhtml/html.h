da8adb5e4b9320472aa7892177dcfdb268fac0ad
NeoYang <nerosoft@outlook.com>
2020-12-20 00:34:54 +0800

    [XingfengYang] #LibHtml/CSS

--- SourceCode/Libraries/LibHTML/include/libhtml/html.h
+++ SourceCode/Libraries/LibHTML/include/libhtml/html.h
@@ -5,4 +5,30 @@
 #ifndef SYNESTIAOS_HTML_H
 #define SYNESTIAOS_HTML_H
 
+typedef struct HTMLAttribute {
+    char *name;
+    char *value;
+} HTMLAttribute;
+
+typedef struct HTMLElement {
+    char *name;
+    struct HTMLAttribute *attributes;
+    struct HTMLElement *children;
+} HTMLElement;
+
+
+typedef HTMLElement *(*HTMLParserOperationParse)(struct HTMLParser *parser, const char *htmlStr);
+
+typedef struct HTMLParserOperations {
+    HTMLParserOperationParse parse;
+} HTMLParserOperations;
+
+
+typedef struct HTMLParser {
+    struct HTMLElement *root;
+    HTMLParserOperations operations;
+} HTMLParser;
+
+HTMLParser *html_parser_create(struct HTMLParser *parser);
+
 #endif //SYNESTIAOS_HTML_H
--- SourceCode/Libraries/LibHTML/src/html.c
+++ SourceCode/Libraries/LibHTML/src/html.c
@@ -1,4 +1,27 @@
 //
 // Created by XingfengYang on 2020/12/19.
 //
+#include "libhtml/html.h"
+#include "libc/stdint.h"
 
+HTMLElement *html_parser_default_parse_element(struct HTMLParser *parser, const char* htmlStr, uint32_t pos){
+
+}
+
+HTMLElement *html_parser_default_parse(struct HTMLParser *parser, const char *htmlStr){
+    const char* str = "<root id=\"rootId\" class=\"root\">"
+                        "<div name=\"container\">"
+                            "<p>test</p>"
+                        "</div>"
+                        "<ul>"
+                            "<li>li1</li>"
+                            "<li>li1</li>"
+                        "</ul>"
+                      "</root>";
+
+    return html_parser_default_parse_element(parser,str,0);
+}
+
+HTMLParser* html_parser_create(struct HTMLParser* parser){
+    parser->operations.parse = html_parser_default_parse;
+}
