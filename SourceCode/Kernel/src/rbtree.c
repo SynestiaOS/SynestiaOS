eba4e164e874de23a396da360cd9b1f050ea11b1
NeoYang <nerosoft@outlook.com>
2020-12-29 20:07:11 +0800

    [XingfengYang] #RBTREE: fix a bug

--- SourceCode/Kernel/src/rbtree.c
+++ SourceCode/Kernel/src/rbtree.c
@@ -15,6 +15,9 @@
 
 RBNode *rbtree_default_get_min(RBTree *tree) {
     RBNode *tmp = tree->root;
+    if (tmp == nullptr) {
+        return nullptr;
+    }
     while (tmp->left != nullptr) {
         tmp = tmp->left;
     }
@@ -23,6 +26,9 @@ RBNode *rbtree_default_get_min(RBTree *tree) {
 
 RBNode *rbtree_default_get_max(RBTree *tree) {
     RBNode *tmp = tree->root;
+    if (tmp == nullptr) {
+        return nullptr;
+    }
     while (tmp->right != nullptr) {
         tmp = tmp->right;
     }
@@ -88,6 +94,7 @@ RBNode *rbtree_default_remove(RBTree *tree, RBNode *node) {
 }
 
 RBTree *rb_tree_init(RBTree *tree) {
+    tree->root = nullptr;
     tree->operations.getMax = (RbTreeGetMaxNode) rbtree_default_get_max;
     tree->operations.getMin = (RbTreeGetMinNode) rbtree_default_get_min;
     tree->operations.insert = (RBTreeInsertNode) rbtree_default_insert;
