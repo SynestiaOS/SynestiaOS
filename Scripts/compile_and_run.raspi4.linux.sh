6b5f2bdc3eeaddb3f9bf14fa4f26a3e238544506
shifu <shifu.wu@outlook.com>
2020-12-30 00:38:15 +0800

    scripts realpath fix

--- Scripts/compile_and_run.raspi4.linux.sh
+++ Scripts/compile_and_run.raspi4.linux.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(dirname "$0")
+BASEDIR=$(realpath "$(dirname "$0")")
 BUILD_DIR="${BASEDIR}"/buildRaspi4
 
 rm -rf "${BUILD_DIR}"
 mkdir "${BUILD_DIR}"
 cd "${BUILD_DIR}" || exit
-cmake -DCMAKE_TOOLCHAIN_FILE="${BASEDIR}"/../../CMake/ToolchainLinuxArm.cmake -DARCH=arm -DPLATFORM=pi4 "${BASEDIR}/../.."
+cmake -DCMAKE_TOOLCHAIN_FILE="${BASEDIR}"/../CMake/ToolchainLinuxArm.cmake -DARCH=arm -DPLATFORM=pi4 "${BASEDIR}/.."
 VERBOSE=1 make
