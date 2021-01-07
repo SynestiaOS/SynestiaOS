4561e3ed2d803d452b2f17759c73b8959da848e5
NeoYang <nerosoft@outlook.com>
2021-01-07 11:33:32 +0800

    [XingfengYang] #BUS: there is a good driver bus design in my head.

--- SourceCode/Kernel/src/bus.c
+++ SourceCode/Kernel/src/bus.c
@@ -4,6 +4,22 @@
 
 #include "kernel/bus.h"
 
-DriverServiceBus *driver_service_bus_create(struct DriverServiceBus *driverServiceBus) {
 
+void driver_service_bus_default_unregister_driver(struct DriverServiceBus *bus, Driver driver) {
+
+}
+
+void driver_service_bus_default_register_driver(struct DriverServiceBus *bus, Driver driver) {
+
+}
+
+void driver_service_bus_default_subscribe_driver(struct DriverServiceBus *bus, Driver driver) {
+
+}
+
+
+DriverServiceBus *driver_service_bus_create(struct DriverServiceBus *driverServiceBus) {
+    driverServiceBus->operation.registerDriver = driver_service_bus_default_register_driver;
+    driverServiceBus->operation.unRegisterDriver = driver_service_bus_default_unregister_driver;
+    driverServiceBus->operation.subscribeDriver = driver_service_bus_default_subscribe_driver;
 }
