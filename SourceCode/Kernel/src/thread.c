fa9de8c4ae35b21644ab677349cfa93a6bae488d
NeoYang <nerosoft@outlook.com>
2021-02-06 11:25:43 +0800

    [XingfengYang] N/A：merge from chengao

--- SourceCode/Kernel/src/thread.c
+++ SourceCode/Kernel/src/thread.c
@@ -256,6 +256,7 @@ KernelStatus thread_default_execute(struct Thread *thread, struct Elf *elf) {
 
     thread_init_mm(thread);
     if (thread->operations.isKernelThread(thread)) {
+
     } else {
         thread->memoryStruct.virtualMemory.operations.release(&thread->memoryStruct.virtualMemory);
         thread->memoryStruct.heap.operations.release(&thread->memoryStruct.heap);
@@ -363,7 +364,7 @@ _Noreturn uint32_t *idle_thread_routine(int arg) {
 
 Thread *thread_create_idle_thread(uint32_t cpuNum) {
     Thread *idleThread = thread_create("IDLE", (ThreadStartRoutine) idle_thread_routine, (void *) cpuNum,
-                                       IDLE_PRIORITY, svcModeCPSR());
+                                       IDLE_PRIORITY, sysModeCPSR());
     idleThread->cpuAffinity = cpuNum;
     // 2. idle thread
     idleThread->pid = 0;
