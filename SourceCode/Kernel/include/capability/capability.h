ad577794a9fb8d5a2603b1c3f7075f86827006a2
XingfengYang <nerosoft@outlook.com>
2021-04-16 20:43:49 +0800

    [XingfengYang] Capability: steal capability from sel4

---
+++ SourceCode/Kernel/include/capability/capability.h
@@ -0,0 +1,10 @@
+#ifndef SYNESTIAOS_CAPABILITY_H
+#define SYNESTIAOS_CAPABILITY_H
+
+#include "kernel/stdint.h"
+
+typedef struct Capability{
+    uint8_t rights;
+} Capability;
+
+#endif // SYNESTIAOS_CAPABILITY_H
