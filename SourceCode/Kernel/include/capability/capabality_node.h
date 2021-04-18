ad7a41c83275d0c8143c69937d1f106378122de9
XingfengYang <nerosoft@outlook.com>
2021-04-18 14:00:10 +0800

    [XingfengYang] #Dirmap: operations

--- SourceCode/Kernel/include/capability/capabality_node.h
+++ SourceCode/Kernel/include/capability/capabality_node.h
@@ -3,12 +3,27 @@
 
 #include "kernel/stdint.h"
 
+typedef uint32_t (*DirMapOperationInsert)(DirMap *dirmap, void *data);
+typedef void* (*DirMapOperationGet)(DirMap* dirmap, uint32_t ref);
+
+typedef struct DirMapOperation{
+    DirMapOperationInsert insert;
+    DirMapOperationGet get;
+} DirMapOperation;
+
 // 2-8-8-12
 typedef struct DirMap {
-    uint32_t nums[4];
+    uint32_t maxLevel;
+    uint32_t used[4];
     uint32_t slot[4];
     uint32_t freelist[4];
+
+    DirMapOperation operation;
+
+    uint32_t* ptrToBlock;
 } DirMap;
+
+DirMap* dirmap_create();
 
 typedef struct CapabilityNode {
     DirMap ownTable; // Own ability and authority
