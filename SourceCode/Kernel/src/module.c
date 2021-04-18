20a3d015f8582db3296ae0f0425e32a44458d81e
XingfengYang <nerosoft@outlook.com>
2021-04-18 16:40:41 +0800

    [XingfengYang] #N/a

--- SourceCode/Kernel/src/module.c
+++ SourceCode/Kernel/src/module.c
@@ -11,9 +11,9 @@ extern uint32_t __module_init_end;
 
 void kernel_module_init(){
     ModuleInitFunction function = nullptr;
-    LogInfo("[Module]: init.\n");
-    for(function = &__module_init_start; function < &__module_init_end; function++){
-        if((*function)(nullptr) < 0){
+    for(function = &__module_init_start; function < &__module_init_end; function + sizeof(ModuleInitFunction)){
+        uint32_t funcAddr = *(uint32_t*)(function);
+        if(((ModuleInitFunction)funcAddr)(nullptr) < 0){
             LogError("[Module]: failed to init module.\n");
         }
     }
