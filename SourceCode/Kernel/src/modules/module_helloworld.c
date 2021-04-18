7846f893b9fb74b7556443d0b17cf470f95cb4cd
XingfengYang <nerosoft@outlook.com>
2021-04-18 17:06:57 +0800

    [Xingfengyang] #N/A

--- SourceCode/Kernel/src/modules/module_helloworld.c
+++ SourceCode/Kernel/src/modules/module_helloworld.c
@@ -2,9 +2,15 @@
 #include "kernel/log.h"
 #include "libc/stdint.h"
 
-uint32_t module_helloworld(void* data){
-    LogInfo("[Module]: module helloworld inited.\n");
+uint32_t module_helloworld1(void* data){
+    LogInfo("[Module]: module helloworld1 inited.\n");
     return 1;
 }
 
-__module_init(module_helloworld);
+uint32_t module_helloworld2(void* data){
+    LogInfo("[Module]: module helloworld2 inited.\n");
+    return 1;
+}
+
+__module_init(module_helloworld1);
+__module_init(module_helloworld2);
