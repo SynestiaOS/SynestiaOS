529193528ccff4d04bc816e9a25035bb95f8b08d
XingfengYang <nerosoft@outlook.com>
2021-04-18 14:45:45 +0800

    [XingfengYang] #Module: may be need kernel module

---
+++ SourceCode/Kernel/include/kernel/module.h
@@ -0,0 +1,14 @@
+//
+// Created by XingfengYang on 2021/4/17.
+//
+
+#ifndef __KERNEL_MODULE_H__
+#define __KERNEL_MODULE_H__
+
+#include <libc/stdint.h> 
+
+typedef uint32_t (*ModuleInitFunction)(void *);
+
+#define __module_init(func) static ModuleInitFunction __module_init_##func __attribute__((section(".moduleinit"))) = func;
+
+#endif // __KERNEL_MODULE_H__
