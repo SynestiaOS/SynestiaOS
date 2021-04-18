c0d8643c560781c14916bb0694e59ec7ff8683a4
XingfengYang <nerosoft@outlook.com>
2021-04-18 16:48:51 +0800

    [XingfengYang] #Module: works

--- SourceCode/Kernel/src/module.c
+++ SourceCode/Kernel/src/module.c
@@ -10,8 +10,9 @@ extern uint32_t __module_init_start;
 extern uint32_t __module_init_end;
 
 void kernel_module_init(){
+    LogInfo("[Module]: init.\n");
     ModuleInitFunction function = nullptr;
-    for(function = &__module_init_start; function < &__module_init_end; function + sizeof(ModuleInitFunction)){
+    for(function = &__module_init_start; function < &__module_init_end; function += sizeof(ModuleInitFunction)){
         uint32_t funcAddr = *(uint32_t*)(function);
         if(((ModuleInitFunction)funcAddr)(nullptr) < 0){
             LogError("[Module]: failed to init module.\n");
